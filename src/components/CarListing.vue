fc56d26e26ccb63aa50359a7f6c045c62ff9b9ab
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-19 11:06:32 -0600

    car listing is really styled up

--- src/components/CarListing.vue
+++ src/components/CarListing.vue
@@ -10,18 +10,24 @@ defineProps({
 <template>
   <div class="row shadow-lg mb-4 car-border">
     <div class="col-md-5 px-0">
-      <img :src="carProp.imgUrl" :alt="`A picture of a ${carProp.year} ${carProp.make} ${carProp.model}`">
+      <img :src="carProp.imgUrl" :alt="`A picture of a ${carProp.year} ${carProp.make} ${carProp.model}`" class="w-100">
     </div>
     <div class="col-md-7">
-      <div class="p-3">
-        <div class="d-flex justify-content-between">
-          <p class="fs-3">{{ carProp.year }} {{ carProp.make }} {{ carProp.model }}</p>
-          <small>{{ carProp.createdAt.toLocaleDateString() }}</small>
+      <div class="p-3 h-100 d-flex flex-column justify-content-between">
+        <div>
+          <div class="d-flex justify-content-between">
+            <p class="fs-3">{{ carProp.year }} {{ carProp.make }} {{ carProp.model }}</p>
+            <small>{{ carProp.createdAt.toLocaleDateString() }}</small>
+          </div>
+          <p class="fs-3">{{ '$' + carProp.price.toLocaleString() }}</p>
+          <p v-if="carProp.description">{{ carProp.description }}</p>
+          <p v-else>A lovely car</p>
+          <p>Engine: {{ carProp.engineType }}</p>
         </div>
-        <p class="fs-3">{{ '$' + carProp.price.toLocaleString() }}</p>
-        <p v-if="carProp.description">{{ carProp.description }}</p>
-        <p v-else>A lovely car</p>
-        <p>Engine: {{ carProp.engineType }}</p>
+        <div class="d-flex justify-content-end align-items-center gap-3">
+          <p class="mb-0">{{ carProp.creator.name }}</p>
+          <img :src="carProp.creator.picture" alt="" class="creator-img">
+        </div>
       </div>
     </div>
   </div>
@@ -29,9 +35,14 @@ defineProps({
 
 
 <style lang="scss" scoped>
+.creator-img {
+  height: 3.7em;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+}
+
 img {
   height: 45dvh;
-  width: 100%;
   object-fit: cover;
 }
 
