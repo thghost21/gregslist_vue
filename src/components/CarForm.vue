e4708502320281a5eaf681706d924d6823cc5424
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-19 11:13:40 -0600

    added car form

---
+++ src/components/CarForm.vue
@@ -0,0 +1,65 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <form onsubmit="app.carsController.createCarListing()">
+    <div class="mb-3">
+      <label for="carMake">Car Make</label>
+      <input id="carMake" name="make" type="text" required maxlength="500" placeholder="Car Make...">
+    </div>
+    <div class="mb-3">
+      <label for="carModel">Car Model</label>
+      <input id="carModel" name="model" type="text" required maxlength="500" placeholder="Car Model...">
+    </div>
+    <div class="mb-3">
+      <label for="carPrice">Car Price</label>
+      <input id="carPrice" name="price" type="number" required min="0" max="1000000" placeholder="0">
+    </div>
+    <div class="mb-3">
+      <label for="carYear">Car Year</label>
+      <input id="carYear" name="year" type="number" required min="1886" max="2025" placeholder="2025">
+    </div>
+    <div class="mb-3">
+      <label for="carImgUrl">Car Image URL</label>
+      <input id="carImgUrl" name="imgUrl" type="url" required maxlength="500" placeholder="Image URL...">
+    </div>
+    <div class="mb-3">
+      <label for="carEngineType">Car Engine Type</label>
+      <select name="engineType" id="carEngineType">
+        <option value="unknown">Unknown</option>
+        <option value="2 stroke">2 Stroke</option>
+        <option value="4 cylinder">4 cylinder</option>
+        <option value="v6">v6</option>
+        <option value="v8">v8</option>
+        <option value="v10">v10</option>
+        <option value="v12">v12</option>
+        <option value="small">small</option>
+        <option value="medium">medium</option>
+        <option value="large">large</option>
+        <option value="chuncko">chuncko</option>
+      </select>
+    </div>
+    <div class="mb-3">
+      <label for="carColor">Car Color</label>
+      <input id="carColor" name="color" type="color" value="#FF0000" class="check-input">
+    </div>
+    <div class="mb-3">
+      <label for="carDescription">Car Description</label>
+      <textarea name="description" id="carDescription" class="w-100" placeholder="Description of the car..."
+        maxlength="500"></textarea>
+    </div>
+    <div class="text-end">
+      <button class="btn btn-outline-danger me-2" type="reset">
+        Reset Form
+      </button>
+      <button class="btn btn-outline-dark" type="submit">
+        Submit
+      </button>
+    </div>
+  </form>
+</template>
+
+
+<style lang="scss" scoped></style>
--- src/pages/CarsPage.vue
+++ src/pages/CarsPage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import CarForm from '@/components/CarForm.vue';
 import CarListing from '@/components/CarListing.vue';
 import { carsService } from '@/services/CarsService.js';
 import { logger } from '@/utils/Logger.js';
@@ -34,6 +35,20 @@ async function getCars() {
     </div>
   </section>
   <section class="container">
+    <div class="row align-items-center">
+      <div class="col-md-6">
+        <CarForm />
+      </div>
+      <div class="col-md-6">
+        <div class="text-center my-3">
+          <img
+            src="https://images.unsplash.com/photo-1522780209919-d8d8b64e0e9e?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="War God hanging out with her brother" class="img-fluid rounded-5 shadow-lg">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container">
     <div class="row">
       <div v-for="car in cars" :key="car.id" class="col-12">
         <CarListing :carProp="car" />
@@ -43,4 +58,10 @@ async function getCars() {
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  max-height: 80dvh;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
